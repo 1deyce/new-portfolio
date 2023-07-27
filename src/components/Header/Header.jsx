b025e580471f21db22891f10a1425e3e4387be6e
Keenan Deyce <keenandeyce@gmail.com>
2023-07-27 16:02:29 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import "../../index.css";
 
 const Header = () => {
     return (
-        <header className='w-full h-20 leading-[80px] flex items-center bg-black'>
+        <header className='w-full h-20 leading-[80px] flex items-center bg-'>
             <div className='container mx-auto'>
                 <div className='flex items-center justify-between'>
                     {/* logo start */}
@@ -12,7 +12,7 @@ const Header = () => {
                         <span className='w-[50px] h-[50px] text-white text-[30px] font-[500] bg-primaryColor rounded-full flex items-center text-center justify-center cursor-pointer border-white border-2'>
                             K
                         </span>
-                        <div className='leading-[20px] ml-2'>
+                        <div className='leading-[20px] pl-2'>
                             <h2 className='text-xl text-primaryColor font-extrabold'>
                                 Keenan Deyce
                             </h2>
@@ -24,23 +24,27 @@ const Header = () => {
                     {/* logo end */}
 
                     {/* navbar start */}
-                    <div className='menu mx-auto'>
-                        <ul className='gap-20 flex'>
+                    <div className='menu mx-auto ml-64'>
+                        <ul className='flex items-center justify-center gap-20'>
                             <li>
-                                <a className='text-white hover:text-primaryColor font-[500]' href="#about">about</a>
+                                <a className='text-white hover:text-primaryColor font-[700]' href="#about">About</a>
                             </li>
                             <li>
-                                <a className='text-white hover:text-primaryColor font-[500]' href="#services">services</a>
+                                <a className='text-white hover:text-primaryColor font-[700]' href="#services">Services</a>
                             </li>
                             <li>
-                                <a className='text-white hover:text-primaryColor font-[500]' href="#portfolio">portfolio</a>
+                                <a className='text-white hover:text-primaryColor font-[700]' href="#portfolio">Portfolio</a>
                             </li>
                             <li>
-                                <a className='text-white hover:text-primaryColor font-[500]' href="#contact">contact</a>
+                                <a className='text-white hover:text-primaryColor font-[700]' href="#contact">Contact</a>
                             </li>
                         </ul>
                     </div>
+                    {/* navbar end */}
 
+                    {/* nav right start */}
+                    
+                    {/* nav right end */}
                 </div>
             </div>
         </header>
