07861ea42be020275d044f35efbb2845971f816f
Keenan Deyce <keenandeyce@gmail.com>
2023-08-17 14:36:19 +0200

    Update Contact.jsx

--- src/components/UI/Contact.jsx
+++ src/components/UI/Contact.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
-import firebaseConfig from "../../firebaseConfig";
+import { initializeApp } from "firebase/app";
 const Contact = () => {
   // Initialize Firebase app
-  const app = firebase.initializeApp(firebaseConfig);
+  const app = initializeApp();
 
   // Initialize Firestore
   const firestore = firebase.firestore(app);
