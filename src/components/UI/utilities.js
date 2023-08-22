7ca5d1fa2ed42ca4a5929efe7d3727823a0e86cd
Keenan Deyce <keenandeyce@gmail.com>
2023-08-22 14:37:52 +0200

    Update utilities.js
    Changed site key + secret key

--- src/components/UI/utilities.js
+++ src/components/UI/utilities.js
@@ -1,2 +1,2 @@
-export const googleCaptchaSiteKey = "6Leh98YnAAAAANVfTTUv9rkl0FLWZLcbClOgvaSe";
-export const googleCaptchaSecretKey = "6Leh98YnAAAAAPD5QtdB1iWkWzB29ELPsKP3t_MY";
+export const googleCaptchaSiteKey = "6LdtSccnAAAAAG2UMMNiJqw-ubOQzuSLfjUtxMct";
+export const googleCaptchaSecretKey = "6LdtSccnAAAAANfFadoH-FjmkQUTnauGdTdcod1D";
