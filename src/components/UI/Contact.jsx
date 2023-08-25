c3dbc4cc51a93f74ee71e52afbe4ea2bf8c6f540
Keenan Deyce <keenandeyce@gmail.com>
2023-08-25 11:08:52 +0200

    Update Contact.jsx

--- src/components/UI/Contact.jsx
+++ src/components/UI/Contact.jsx
@@ -3,7 +3,7 @@ import { collection, addDoc } from "firebase/firestore/lite";
 import { db } from "./firebaseConfig";
 import ReCAPTCHA from "react-google-recaptcha";
 
-const key = "6Leh98YnAAAAANVfTTUv9rkl0FLWZLcbClOgvaSe";
+const key = "6Leh98YnAAAAAPD5QtdB1iWkWzB29ELPsKP3t_MY";
 const Contact = () => {
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
