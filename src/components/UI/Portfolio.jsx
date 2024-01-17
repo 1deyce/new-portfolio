315d1ce0ee83f0d7109a5870fd20e2ec3a1e10ac
Keenan Deyce <keenandeyce@gmail.com>
2024-01-17 09:35:03 +0200

    Update
    Added new project to portfolio

--- src/components/UI/Portfolio.jsx
+++ src/components/UI/Portfolio.jsx
@@ -4,13 +4,14 @@ import "../../styles/modal.css";
 import Img from "../../assets/images/FWG.png";
 import Img1 from "../../assets/images/blogapp.png";
 import Img2 from "../../assets/images/bankapp.png";
+import Img3 from "../../assets/images/ecotrack.png";
 import Modal from "react-modal";
 
 const Portfolio = () => {
   const [isModalOpen1, setIsModalOpen1] = useState(false);
   const [isModalOpen2, setIsModalOpen2] = useState(false);
   const [isModalOpen3, setIsModalOpen3] = useState(false);
-
+  const [isModalOpen4, setIsModalOpen4] = useState(false);
   useEffect(() => {
     Modal.setAppElement("#root");
   }, []);
@@ -39,6 +40,14 @@ const Portfolio = () => {
     setIsModalOpen3(false);
   };
 
+  const openModal4 = () => {
+    setIsModalOpen4(true);
+  };
+
+  const closeModal4 = () => {
+    setIsModalOpen4(false);
+  };
+
   return (
     <section id="portfolio" className="bg-white dark:bg-black pt-28 pb-20">
       <div className="container mb-20">
@@ -49,8 +58,7 @@ const Portfolio = () => {
             </span>
           </h2>
           <p className="lg:max-w-[1000px] lg:mx-auto text-headingColor dark:text-white font-[500] text-[16px] leading-7 pt-6">
-            Whether it's web development, graphic design, or any other creative
-            endeavor, I always strive for excellence and attention to detail.
+            Whether it's web development, graphic design, or a full-stack application, I always strive for excellence and attention to detail.
             Here, you'll find a showcase of my latest projects, where I've
             poured my creativity and expertise.
           </p>
@@ -229,7 +237,7 @@ const Portfolio = () => {
               Fitness Trainer Website
             </h5>
             <p className="mb-3 font-normal text-secondaryColor dark:text-white">
-              React, Tailwind CSS
+              React.js, Tailwind CSS
             </p>
             <a
               href="https://lighthearted-marigold-9a1a80.netlify.app"
@@ -256,6 +264,86 @@ const Portfolio = () => {
             </a>
             <a
               href="https://github.com/1deyce/Gym-website"
+              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white dark:text-black bg-black rounded-lg hover:bg-primaryColor hover:text-black focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-white dark:hover:bg-primaryColor dark:hover:text-white dark:focus:ring-blue-800"
+              target="_blank"
+              rel="noreferrer"
+            >
+              View Source Code
+              <svg
+                className="w-3.5 h-3.5 ml-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+        {/* fourth card */}
+        <div className="max-w-sm bg-white dark:border-black rounded-2xl dark:bg-black hover:shadow-md">
+          <img
+            className="cursor-pointer w-full h-[210px] hover:scale-110 transition duration-300 ease-in-out rounded-lg pt-1 px-1"
+            src={Img3}
+            alt=""
+            onClick={openModal4}
+          />
+          <Modal
+            isOpen={isModalOpen4}
+            onRequestClose={closeModal4}
+            contentLabel="Image Modal"
+            className="modal bg-black"
+            overlayClassName="modal-overlay"
+          >
+            <div className="modal-content">
+              <img src={Img3} alt="" className="modal-image" />
+              <button
+                onClick={closeModal4}
+                className="absolute top-0.5 right-3 text-gray-300 hover:text-primaryColor text-[30px] hover:scale-125"
+              >
+                <span>&times;</span>
+              </button>
+            </div>
+          </Modal>
+          <div className="p-5 text-center">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-primaryColor dark:text-primaryColor">
+              Waste Management App
+            </h5>
+            <p className="mb-3 font-normal text-secondaryColor dark:text-white">
+              MERN stack, Tailwind CSS 
+            </p>
+            <a
+              href="https://celadon-llama-2fa0fe.netlify.app"
+              className="inline-flex items-center px-3 mx-3 py-2 text-sm font-medium text-center text-white dark:text-black bg-black rounded-lg hover:bg-primaryColor hover:text-black focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-white dark:hover:bg-primaryColor dark:hover:text-white dark:focus:ring-blue-800"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Live Site
+              <svg
+                className="w-3.5 h-3.5 ml-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+            <a
+              href="https://github.com/1deyce/EcoTrack"
               className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white dark:text-black bg-black rounded-lg hover:bg-primaryColor hover:text-black focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-white dark:hover:bg-primaryColor dark:hover:text-white dark:focus:ring-blue-800"
               target="_blank"
               rel="noreferrer"
