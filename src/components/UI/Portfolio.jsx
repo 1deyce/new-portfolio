106a97943df03f698b88f675e97d557ce998a84f
Keenan Deyce <124916049+1deyce@users.noreply.github.com>
2024-05-17 16:02:50 +0200

    Update Portfolio.jsx

--- src/components/UI/Portfolio.jsx
+++ src/components/UI/Portfolio.jsx
@@ -240,7 +240,7 @@ const Portfolio = () => {
               React.js, Tailwind CSS
             </p>
             <a
-              href="https://lighthearted-marigold-9a1a80.netlify.app"
+              href="https://fitnesswithgaby.co.za"
               className="inline-flex items-center px-3 mx-3 py-2 text-sm font-medium text-center text-white dark:text-black bg-black rounded-lg hover:bg-primaryColor hover:text-black focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-white dark:hover:bg-primaryColor dark:hover:text-white dark:focus:ring-blue-800"
               target="_blank"
               rel="noreferrer"
