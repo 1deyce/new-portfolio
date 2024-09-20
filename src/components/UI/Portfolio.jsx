782e7752d5ae98a4aab904029555fbb7ae123e5b
1deyce <124916049+1deyce@users.noreply.github.com>
2024-09-20 16:58:17 +0200

    Update Portfolio.jsx

--- src/components/UI/Portfolio.jsx
+++ src/components/UI/Portfolio.jsx
@@ -320,7 +320,7 @@ const Portfolio = () => {
               MERN stack, Tailwind CSS 
             </p>
             <a
-              href="https://celadon-llama-2fa0fe.netlify.app"
+              href="https://ecotrack-solutions.netlify.app/"
               className="inline-flex items-center px-3 mx-3 py-2 text-sm font-medium text-center text-white dark:text-black bg-black rounded-lg hover:bg-primaryColor hover:text-black focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-white dark:hover:bg-primaryColor dark:hover:text-white dark:focus:ring-blue-800"
               target="_blank"
               rel="noreferrer"
